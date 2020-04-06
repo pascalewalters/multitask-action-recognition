147173084eb5b72fac9f628767b1ee7f13859c89
Pascale Walters <pascalewalters@gmail.com>
2020-04-06 13:23:29 -0400

    update options

--- opts.py
+++ opts.py
@@ -14,7 +14,7 @@ randomseed = 0
 
 # directory containing dataset annotation files; this anno_n_splits_dir make the full path
 # dataset_dir = '/home/pascale/Documents/courses/CS886/MTL-AQA/MTL-AQA_dataset_release/Ready_2_Use/'
-dataset_dir = '/home/pascale/Documents/courses/CS886/output_clips/'
+dataset_dir = '/home/pascale/Documents/courses/CS886/final_project/output_clips/'
 
 # directory tp store train/test split lists and annotations
 # anno_n_splits_dir = dataset_dir + 'smaller_training_sets/size_140/'
@@ -25,11 +25,11 @@ anno_n_splits_dir = dataset_dir + 'train_data'
 dataset_frames_dir = dataset_dir + 'frames/'
 
 # directory for saving output
-saving_dir = 'output2/'
+saving_dir = 'output8/'
 
 # sample length in terms of no of frames
 # sample_length = 103
-sample_length = 110
+sample_length = 25
 
 # input data dims; C3D-AVG:112; MSCADC: 180
 C, H, W = 3,112,112
@@ -54,6 +54,7 @@ caption_lstm_dim_word = 512
 caption_lstm_dim_vid = 8192 # C3D-AVG: 8192; MSCADC: 1200
 # caption_lstm_dim_vid = 1200#8192# C3D-AVG: 8192; MSCADC: 1200
 caption_lstm_cell_type = 'gru'
+# caption_lstm_cell_type = 'lstm'
 caption_lstm_num_layers = 2
 caption_lstm_dropout = 0.5
 caption_lstm_lr = 0.0001
