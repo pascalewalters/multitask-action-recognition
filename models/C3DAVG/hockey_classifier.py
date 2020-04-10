77ce296e78147f9405a248929e458e28a882ef9c
Pascale Walters <pascalewalters@gmail.com>
2020-04-10 11:31:42 -0400

    fix comments

--- models/C3DAVG/hockey_classifier.py
+++ models/C3DAVG/hockey_classifier.py
@@ -1,14 +1,3 @@
-# Author: Paritosh Parmar (https://github.com/ParitoshParmar)
-# Code used in the following, also if you find it useful, please consider citing the following:
-#
-# @inproceedings{parmar2019and,
-#   title={What and How Well You Performed? A Multitask Learning Approach to Action Quality Assessment},
-#   author={Parmar, Paritosh and Tran Morris, Brendan},
-#   booktitle={Proceedings of the IEEE Conference on Computer Vision and Pattern Recognition},
-#   pages={304--313},
-#   year={2019}
-# }
-
 import torch
 import torch.nn as nn
 import numpy as np
@@ -21,7 +10,6 @@ class hockey_classifier(nn.Module):
     def __init__(self):
         super(hockey_classifier, self).__init__()
 
-        # First attempt: binary classification for each event type
         self.fc_switch = nn.Linear(4096, 10)
         self.fc_advance = nn.Linear(4096, 10)
         self.fc_faceoff = nn.Linear(4096, 10)
